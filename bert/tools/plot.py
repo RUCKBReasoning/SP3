a33d9fbe75be15d5eb44d053f389f050ec004ccc
huyuxuan <huyuxuan1999@ruc.edu.cn>
2023-10-08 21:46:03 +0800

    refactor

--- bert/tools/plot.py
+++ bert/tools/plot.py
@@ -10,6 +10,10 @@ from modeling.modeling_packed_bert import (
 )
 from typing import List
 
+font = {
+    "size": 17
+}
+
 def draw_figure1(p_config, name):
     per_layer_config: List[PackedBertLayerConfig] = p_config.per_layer_config
     hidden_size = p_config.hidden_size
@@ -43,17 +47,54 @@ def draw_figure1(p_config, name):
         # ax.bar_label(rects, padding=3)
         multiplier += 1
 
-    ax.set_ylabel('percentage (%)')
-    ax.set_title('Intermediate dimensions of each layer in PLM')
+    ax.set_ylabel('percentage (%)', fontdict=font)
+    ax.set_title('Intermediate dimensions of each layer in PLM', fontdict=font)
     ax.set_xticks(x + width, x_values)
-    ax.legend(loc='upper right', ncols=3)
+    ax.legend(loc='upper right', ncols=3, fontsize=16)
+    ax.set_ylim(0, 25)
+
+    plt.savefig("tools/figures/{}.png".format(name))
+    plt.close()
+
+def draw_figure2(p_config, name):
+    per_layer_config: List[PackedBertLayerConfig] = p_config.per_layer_config
+    num_heads = p_config.num_attention_heads
+
+    hidden_dim = []
+    for layer in per_layer_config:
+        hidden_dim.append(layer.num_heads * (0 if layer.prune_attn else 1))
+
+    x_values = tuple(i for i in range(1, 13))
+    group_y_values = {
+        "number of attention heads": [
+            x / num_heads * 100 
+                for x in hidden_dim
+        ],
+    }
+
+    x = np.arange(len(x_values))  # the label locations
+    width = 0.5  # the width of the bars
+    multiplier = 0
+
+    fig, ax = plt.subplots(layout='constrained')
+
+    for attribute, measurement in group_y_values.items():
+        offset = width * multiplier
+        rects = ax.bar(x + offset, measurement, width, label=attribute)
+        # ax.bar_label(rects, padding=3)
+        multiplier += 1
+
+    ax.set_ylabel('percentage (%)', fontdict=font)
+    ax.set_title('Number of attention heads of each layer in PLM', fontdict=font)
+    ax.set_xticks(x, x_values)
+    ax.legend(loc='upper right', ncols=3, fontsize=16)
     ax.set_ylim(0, 100)
 
     plt.savefig("tools/figures/{}.png".format(name))
     plt.close()
 
 
-def draw_figure2(p_config, name):
+def draw_figure3(p_config, name):
     per_layer_config: List[PackedBertLayerConfig] = p_config.per_layer_config
     hidden_size = p_config.hidden_size
 
@@ -82,48 +123,10 @@ def draw_figure2(p_config, name):
         # ax.bar_label(rects, padding=3)
         multiplier += 1
 
-    ax.set_ylabel('percentage (%)')
-    ax.set_title('Hidden dimensions of each layer in PLM')
+    ax.set_ylabel('percentage (%)', fontdict=font)
+    ax.set_title('Hidden dimensions of each layer in PLM', fontdict=font)
     ax.set_xticks(x, x_values)
-    ax.legend(loc='upper right', ncols=3)
-    ax.set_ylim(0, 100)
-
-    plt.savefig("tools/figures/{}.png".format(name))
-    plt.close()
-
-
-def draw_figure3(p_config, name):
-    per_layer_config: List[PackedBertLayerConfig] = p_config.per_layer_config
-    num_heads = p_config.num_attention_heads
-
-    hidden_dim = []
-    for layer in per_layer_config:
-        hidden_dim.append(layer.num_heads * (0 if layer.prune_attn else 1))
-
-    x_values = tuple(i for i in range(1, 13))
-    group_y_values = {
-        "number of attention heads": [
-            x / num_heads * 100 
-                for x in hidden_dim
-        ],
-    }
-
-    x = np.arange(len(x_values))  # the label locations
-    width = 0.5  # the width of the bars
-    multiplier = 0
-
-    fig, ax = plt.subplots(layout='constrained')
-
-    for attribute, measurement in group_y_values.items():
-        offset = width * multiplier
-        rects = ax.bar(x + offset, measurement, width, label=attribute)
-        # ax.bar_label(rects, padding=3)
-        multiplier += 1
-
-    ax.set_ylabel('percentage (%)')
-    ax.set_title('Number of attention heads of each layer in PLM')
-    ax.set_xticks(x, x_values)
-    ax.legend(loc='upper right', ncols=3)
+    ax.legend(loc='upper right', ncols=3, fontsize=16)
     ax.set_ylim(0, 100)
 
     plt.savefig("tools/figures/{}.png".format(name))
