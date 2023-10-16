a404575716eba0b281d9391aba49667dc44c962d
huyuxuan <huyuxuan1999@ruc.edu.cn>
2023-10-16 12:22:16 +0800

    refactor

--- bert/tools/plot.py
+++ bert/tools/plot.py
@@ -11,25 +11,64 @@ from modeling.modeling_packed_bert import (
 from typing import List
 
 font = {
-    "size": 17
+    "size": 20
 }
 
-def draw_figure1(p_config, name, dataset_name):
+def draw_figure_inter(p_config, name, dataset_name):
+    per_layer_config: List[PackedBertLayerConfig] = p_config.per_layer_config
+    hidden_size = p_config.hidden_size
+
+    hidden_dim = []
+    for layer in per_layer_config:
+        hidden_dim.extend([
+            layer.attn_output_dim,
+            layer.ffn_output_dim,
+        ])
+
+    x_values = tuple(i for i in range(1, 13))
+    group_y_values = {
+        "$d$ of the MHA block": [x / hidden_size * 100 for x in hidden_dim][0::2],
+        "$d$ of the FFN block": [x / hidden_size * 100 for x in hidden_dim][1::2],
+    }
+
+    x = np.arange(len(x_values))  # the label locations
+    width = 0.25  # the width of the bars
+    multiplier = 0
+
+    fig, ax = plt.subplots(layout='constrained')
+
+    for attribute, measurement in group_y_values.items():
+        offset = width * multiplier
+        rects = ax.bar(x + offset, measurement, width, label=attribute)
+        # ax.bar_label(rects, padding=3)
+        multiplier += 1
+
+    ax.set_ylabel('Sparsity (%)', fontdict=font)
+    ax.set_xlabel('# Layers', fontdict=font)
+    ax.set_xticks(x + width / 2.0, x_values)
+    ax.legend(loc='upper right', ncols=1, fontsize=16)
+    ax.set_ylim(0, 100)
+
+    plt.savefig("tools/figures/{}/{}.png".format(dataset_name, name))
+    plt.close()
+
+
+def draw_figure_intra(p_config, name, dataset_name):
     per_layer_config: List[PackedBertLayerConfig] = p_config.per_layer_config
     hidden_size = p_config.hidden_size
     ffn_size = p_config.intermediate_size
 
     x_values = tuple(i for i in range(1, 13))
     group_y_values = {
-        "qk": tuple(
+        r"$d_{\rm QK}$": tuple(
             layer.qk_dim / hidden_size * 100 * (0 if layer.prune_attn else 1)
                 for layer in per_layer_config
         ),
-        "vo": tuple(
+        r"$d_{\rm VO}$": tuple(
             layer.vo_dim / hidden_size * 100 * (0 if layer.prune_attn else 1)
                 for layer in per_layer_config
         ),
-        "ffn": tuple(
+        r"$d_{f}$": tuple(
             layer.ffn_dim / ffn_size * 100 * (0 if layer.prune_ffn else 1)
                 for layer in per_layer_config
         ),
@@ -47,8 +86,8 @@ def draw_figure1(p_config, name, dataset_name):
         # ax.bar_label(rects, padding=3)
         multiplier += 1
 
-    ax.set_ylabel('percentage (%)', fontdict=font)
-    ax.set_title('Intermediate dimensions of each layer in PLM', fontdict=font)
+    ax.set_ylabel('Sparsity (%)', fontdict=font)
+    ax.set_xlabel('# Layers', fontdict=font)
     ax.set_xticks(x + width, x_values)
     ax.legend(loc='upper right', ncols=3, fontsize=16)
     ax.set_ylim(0, 25)
@@ -56,7 +95,8 @@ def draw_figure1(p_config, name, dataset_name):
     plt.savefig("tools/figures/{}/{}.png".format(dataset_name, name))
     plt.close()
 
-def draw_figure2(p_config, name, dataset_name):
+
+def draw_figure_heads(p_config, name, dataset_name):
     per_layer_config: List[PackedBertLayerConfig] = p_config.per_layer_config
     num_heads = p_config.num_attention_heads
 
@@ -84,49 +124,9 @@ def draw_figure2(p_config, name, dataset_name):
         # ax.bar_label(rects, padding=3)
         multiplier += 1
 
-    ax.set_ylabel('percentage (%)', fontdict=font)
-    ax.set_title('Number of attention heads of each layer in PLM', fontdict=font)
+    ax.set_ylabel('Sparsity (%)', fontdict=font)
+    ax.set_xlabel('# Layers', fontdict=font)
     ax.set_xticks(x, x_values)
-    ax.legend(loc='upper right', ncols=3, fontsize=16)
-    ax.set_ylim(0, 100)
-
-    plt.savefig("tools/figures/{}/{}.png".format(dataset_name, name))
-    plt.close()
-
-
-def draw_figure3(p_config, name, dataset_name):
-    per_layer_config: List[PackedBertLayerConfig] = p_config.per_layer_config
-    hidden_size = p_config.hidden_size
-
-    hidden_dim = []
-    for layer in per_layer_config:
-        hidden_dim.extend([
-            layer.input_dim,
-            layer.attn_output_dim
-        ])
-    hidden_dim.append(per_layer_config[-1].ffn_output_dim)
-
-    x_values = tuple(i for i in range(0, 25))
-    group_y_values = {
-        "hidden_dim": [x / hidden_size * 100 for x in hidden_dim],
-    }
-
-    x = np.arange(len(x_values))  # the label locations
-    width = 0.5  # the width of the bars
-    multiplier = 0
-
-    fig, ax = plt.subplots(layout='constrained')
-
-    for attribute, measurement in group_y_values.items():
-        offset = width * multiplier
-        rects = ax.plot(x + offset, measurement, label=attribute, linewidth=2)
-        # ax.bar_label(rects, padding=3)
-        multiplier += 1
-
-    ax.set_ylabel('percentage (%)', fontdict=font)
-    ax.set_title('Hidden dimensions of each layer in PLM', fontdict=font)
-    ax.set_xticks(x, x_values)
-    ax.legend(loc='upper right', ncols=3, fontsize=16)
     ax.set_ylim(0, 100)
 
     plt.savefig("tools/figures/{}/{}.png".format(dataset_name, name))
@@ -139,9 +139,13 @@ def run_glue():
         p_model_config_path = os.path.join(s_output_dir, "pmodel_config.bin")
         p_config: PackedBertConfig = torch.load(p_model_config_path)
 
-        draw_figure1(p_config, "exp-fig1", dataset_name)
-        draw_figure2(p_config, "exp-fig2", dataset_name)
-        draw_figure3(p_config, "exp-fig3", dataset_name)
+        dir_path = "tools/figures/{}".format(dataset_name)
+        os.makedirs(dir_path, exist_ok=True)
+
+        draw_figure_inter(p_config, "exp-fig1", dataset_name)
+        draw_figure_intra(p_config, "exp-fig2", dataset_name)
+        draw_figure_heads(p_config, "exp-fig3", dataset_name)
+
 
 def run_squad():
     dataset_name = "squad"
@@ -149,9 +153,12 @@ def run_squad():
     p_model_config_path = os.path.join(s_output_dir, "pmodel_config.bin")
     p_config: PackedBertConfig = torch.load(p_model_config_path)
 
-    draw_figure1(p_config, "exp-fig1", dataset_name)
-    draw_figure2(p_config, "exp-fig2", dataset_name)
-    draw_figure3(p_config, "exp-fig3", dataset_name)
+    dir_path = "tools/figures/squad"
+    os.makedirs(dir_path, exist_ok=True)
+
+    draw_figure_inter(p_config, "exp-fig1", dataset_name)
+    draw_figure_intra(p_config, "exp-fig2", dataset_name)
+    draw_figure_heads(p_config, "exp-fig3", dataset_name)
 
 
 run_glue()
